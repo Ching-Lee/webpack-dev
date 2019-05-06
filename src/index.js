c0c1309fc1e3f366fd2c4127cd349dee168abe84
Ching-Lee <lijing2012stu@163.com>
2019-05-06 22:08:39 +0800

    2019/5/6 增加对js的相关操作，包括ES6语法转换，ESLint校验，图片打包，打包资源分类

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './index.css'
-//console.log('1111');
+
+// 在js中打包时需要import图片进来
+// import logo from './logo.png'
+
+// 用于测试ES6语法的转换
+let fn=()=>{
+	// eslint语法检验时会检验不能console
+	//console.log('log');
+}
+fn();
+
+// 装饰器测试
+//@log
+// 用于测试ES7中class语法的转换
+class T{
+	a=1;
+}
+let t=new T();
+t.a=5;
+console.log(t.a);
+
+console.log('aaa'.includes('a'));
+
+// 打包图片
+// 1.在js中创建图片并引用
+// file-loader默认会在内部生成一张图片到build目录下，并且把生成的图片名字返回回来
+// let image=new Image();
+// console.log(logo);
+// image.src=logo;
+// document.body.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,32 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
  devServer:{ //开发服务器的配置
     port:3000, //端口号
- },
+  },
 
  // 优化项，包括js压缩，css压缩
  optimization:{
-	minimizer:[
-	new UglifyJsPlugin({
-		cache:true,
-		parallel:true,
-		sourceMap:true
-	}),
-	new  OptimizeCss() 
-	]
+   minimizer:[
+   new UglifyJsPlugin({
+    cache:true,
+    parallel:true,
+    sourceMap:true
+  }),
+   new  OptimizeCss() 
+   ]
  },
 
 
  entry: './src/index.js', //入口文件
  output: {
     filename: 'bundle.js',// 出口文件
-    path: path.resolve(__dirname, 'dist')
- },
+    path: path.resolve(__dirname, 'dist'),
+    //打包完之后发布到cdn上，所有资源根目录都是它
+    //publicPath:'http://ww.123.cn'
+  },
 
 
   // 数组 放着所欲的Webpack插件
- plugins:[
+  plugins:[
   // 将html打包到dist/html
   new HtmlWebpackPlugin({
   		template:'./src/index.html',// 源文件
@@ -42,25 +44,82 @@ module.exports = {
   	}),
   // 将css抽象到dist/css
   new MiniCssExtractPlugin({
-  	filename:'style.css'
-   })
+  	filename:'css/style.css'
+  })
   ],
 
 // 通过此模块，能够将在insex.js中import的所有.css结尾的文件，
 // 打包到html中的head标签里面
- module: {
-  rules: [
-	 {
-		test: /\.css$/,
-		use: [
+module: {
+  rules: [ //loader默认是从右向左，从下到上执行。
+  //用户处理js语法校验
+  {
+    test:/\.js$/,
+    use:{
+      loader:'eslint-loader',
+      options:{
+        enforce:'pre' //previous post
+      }
+    }
+  },
+
+  // 用于转换es6语法
+  {
+    test:/\.js$/,
+    use:{
+      loader:'babel-loader',
+      options:{
+        presets:[
+        '@babel/preset-env'
+        ],
+        // 支持ES7装饰器，class语法
+        plugins:[
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+        "@babel/plugin-transform-runtime"
+        ]
+      }
+    },
+    include:path.resolve(__dirname,'src'),
+    exclude:/node_modules/
+  },
+  // css相关规则
+  {
+    test: /\.css$/,
+    use: [
+    // 用于将生成的css以内联的方式插入到html
 		// 'style-loader',
 		MiniCssExtractPlugin.loader,
-		'css-loader',
-		'postcss-loader',
-		]
-	 }
-	]
- },
+    // 用于打包生成css
+    'css-loader',
+    // 用于为css中的内容添加前缀
+    'postcss-loader',
+    ]
+  },
+  // 图片相关规则
+   {
+    test:/\.(png|jpg|gif)$/,
+    //use:'file-loader'
+    // 做一个限制，当图片小于多少时，使用base64来转化，
+    // 否则使用file-loader产生真实图片
+    use:{
+      loader:'url-loader',
+      options:{
+        //limit:200*1024,
+        limit:1,
+        outputPath:'/img/',
+        // 也可以根据情况单独添加
+        publicPath:'http://ww.123.cn'
+      }
+    }
+   },
+   // 在html中能使用图片
+   {
+    test:/\.html$/,
+    use:'html-withimg-loader'
+   }
+  ]
+},
 
   //mode默认有两种 开发模式：development(方便开发时看)  生产模式：production(默认)
   mode:'production'  
